efb785d1ebea5e45410b8feac6c7ea06d159ef08
Yuri Nakashima <yuri310@gmail.com>
2017-01-14 14:43:50 -0800

    updated product attributes coming from Rails, still get product pricelable to show up. Added search box that doesnt do anything

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    toggleHidden(product) {
+      product.toggleProperty('hidden');
+      console.log(">>>>toggleHidden button works!", product);
+      product.save();
+    }
+  }
 });
--- app/models/product.js
+++ app/models/product.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  image: DS.attr('string'),
   name: DS.attr('string'),
   retailer: DS.attr('string'),
-  price: DS.attr('number'),
+  priceLabel: DS.attr('string'),
+  url: DS.attr('string'),
   hidden: DS.attr('boolean')
 });
