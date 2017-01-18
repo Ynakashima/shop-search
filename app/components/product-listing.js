74a92e95c006a0cd74f09fc0d3a0d578d2f640f3
Yuri Nakashima <yuri310@gmail.com>
2017-01-18 13:47:55 -0800

    hide product function works again on front end, moved logic to product-list component

--- app/components/product-listing.js
+++ app/components/product-listing.js
@@ -5,6 +5,12 @@ export default Ember.Component.extend({
   actions: {
     toggleImageSize() {
       this.toggleProperty('isWide');
+    },
+
+    toggleHidden(product) {
+      product.toggleProperty('hidden');
+      console.log(">>>>toggleHidden button works!", product);
+      product.save();
     }
   }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   // queryParams: ['search'],
   // search: "",
-  // actions: {
-  //   toggleHidden(product) {
-  //     product.toggleProperty('hidden');
-  //     console.log(">>>>toggleHidden button works!", product);
-  //     product.save();
-  //   }
-  // }
+  
 });
