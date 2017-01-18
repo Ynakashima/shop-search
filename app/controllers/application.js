e048e7c76d31c646561881484f034f394ccff05f
Yuri Nakashima <yuri310@gmail.com>
2017-01-18 13:26:01 -0800

    added product-list component so that user can interact with product, images get larger with user click action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['search'],
-  search: "",
-  actions: {
-    toggleHidden(product) {
-      product.toggleProperty('hidden');
-      console.log(">>>>toggleHidden button works!", product);
-      product.save();
-    }
-  }
+  // queryParams: ['search'],
+  // search: "",
+  // actions: {
+  //   toggleHidden(product) {
+  //     product.toggleProperty('hidden');
+  //     console.log(">>>>toggleHidden button works!", product);
+  //     product.save();
+  //   }
+  // }
 });
