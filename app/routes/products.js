76d1731d02f510d3835cec68d9f5708b88047728
Yuri Nakashima <yuri310@gmail.com>
2017-01-23 18:50:54 -0800

    styling

--- app/routes/products.js
+++ app/routes/products.js
@@ -7,9 +7,6 @@ export default Ember.Route.extend({
       replace: true
     }
   },
-  // model: function(param) {
-  //   return this.get('store').query('product', {name: param });
-  // }
 
   model(params) {
     return this.store.query('product', params);
